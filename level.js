37213c2d3c8b9866944c59e48d248277601da3da
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-11-23 19:06:36 +0100

    refactor mainchar as person

--- level.js
+++ level.js
@@ -3,22 +3,9 @@ class Level {
         this.lvl = lvl;
         this.map = new Map(lvl, spritesheet);
         this.listPolice = [];
-        this.loadListPolice(lvl);
+        
     }
-
-    loadListPolice(lvl) {
-        var sprites = [];
-        // load all movements
-        for(var i = 0; i < spriteSheetPolice.width; i += 16) {
-            sprites.push(spriteSheetPolice.get(i, 0, 20, 48));
-        }
-        this.listPolice.push(new Police([
-            createVector(4,3), 
-            createVector(4,4), 
-            createVector(4,5), 
-            createVector(4,6)], sprites));
-    }
-
+    
     render() {
         this.map.render();
 
--- mainChar.js
+++ mainChar.js
@@ -1,10 +1,9 @@
-class MainChar {
+class MainChar extends Person {
     constructor(spritesheet) {
+        super();
         this.pos = createVector(0,0);
-        this.imgs = []
         this.loadSprites(spritesheet);
         this.currentImg = this.imgs[0];
-        
     }
 
     loadSprites(spritesheet) {
@@ -14,45 +13,4 @@ class MainChar {
         }
     }
 
-    validMove(old) {
-        // only move if not wall
-        if( level.map.isOutOfMap(this.pos.x, this.pos.y)
-            || level.map.wall(this.pos.x, this.pos.y)) {
-                this.pos = old;
-        }
-
-    }
-
-    moveUp() {
-        this.currentImg = this.imgs[1];
-        var oldPos = this.pos.copy();
-        this.pos.add(createVector(0, -1));
-        this.validMove(oldPos);
-    }
-
-    moveDown() {
-        this.currentImg = this.imgs[0];
-        var oldPos = this.pos.copy();
-        this.pos.add(createVector(0, 1));
-        this.validMove(oldPos);
-    }
-
-    moveLeft() {
-        this.currentImg = this.imgs[3];
-        var oldPos = this.pos.copy();
-        this.pos.add(createVector(-1, 0));
-        this.validMove(oldPos);
-    }
-
-    moveRight() {
-        this.currentImg = this.imgs[2];
-        var oldPos = this.pos.copy();
-        this.pos.add(createVector(1, 0));
-        this.validMove(oldPos);
-    }
-
-    render() {
-        imageMode(CORNER);
-        image(this.currentImg, this.pos.x*unit, this.pos.y*unit, unit, unit);
-    }
 }
