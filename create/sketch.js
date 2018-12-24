748e450e162422a00f48b8a26386943fceb60127
Diogo Nunes <diogo@Diogos-MBP.lan>
2018-12-24 10:30:16 +0000

    creating level - init and final pos, walls and police

--- create/sketch.js
+++ create/sketch.js
@@ -3,7 +3,9 @@ var spritesheetGround, spritesheetMainChar, spriteSheetPolice, groundImg, wallIm
 var walls = [];
 var unit = 40, maxRows = 20, maxCols = 20;
 var state;
-var initPos;
+var initPos, finalPos;
+var policeRoute = [];
+var allPoliceRoutes = [];
 
 function preload() {
     spritesheetGround = loadImage("../assets/ground.png");
@@ -26,9 +28,13 @@ function setup() {
 }
 
 function removeLast() {
-    if(state == 'I') {
-        initPos = null;
+    if(state == 'W') {
+        walls.splice(-1,1);
     }
+    else if(state == 'P') {
+        allPoliceRoutes.splice(-1,1);
+    }
+
 }
 
 function keyPressed() {
@@ -45,13 +51,40 @@ function keyPressed() {
 
 function mousePressed() {
     gridPlace = createVector(floor(mouseX/unit), floor(mouseY/unit));
-    console.log(gridPlace);
-    
-    if(state == 'I') {
-        if(gridPlace.x < colsSlider.value() &&
-           gridPlace.y < rowsSlider.value()) {
+    if(gridPlace.x < colsSlider.value() && gridPlace.x >= 0 &&
+        gridPlace.y < rowsSlider.value() && gridPlace.y >= 0) {
+            if(state == 'I') {
                 initPos = gridPlace.copy();
-           }
+                console.log(initPos);
+            }
+            else if(state == 'F') {
+                finalPos = gridPlace.copy();
+            }
+            else if(state == 'W') {
+                walls.push(gridPlace);
+            }
+    }
+}
+
+function mouseDragged() {
+    gridPlace = createVector(floor(mouseX/unit), floor(mouseY/unit));
+    if(gridPlace.x < colsSlider.value() && gridPlace.y < rowsSlider.value()) {
+        if(state == 'P') {
+            for(var i = 0; i < policeRoute.length; i++) {
+                if( (policeRoute[i].x == gridPlace.x) && (policeRoute[i].y == gridPlace.y)) {
+                    return;
+                }
+            }
+            policeRoute.push(gridPlace);
+            console.log(gridPlace);
+        }
+    }
+}
+
+function mouseReleased() {
+    if(policeRoute.length >= 0 && state == 'P') {
+        allPoliceRoutes.push(policeRoute);
+        policeRoute = [];
     }
 }
 
@@ -86,8 +119,61 @@ function renderMainChar() {
     } 
 }
 
+function renderExit() {
+    if(finalPos) {
+        stroke(255);
+        fill(255);
+        textAlign(CENTER);
+        text("EXIT", finalPos.x*unit + unit/2, finalPos.y*unit + unit/2);
+    }
+}
+
+function renderPolice() {
+    for(var i = 0; i < allPoliceRoutes.length; i++) {
+        var p = allPoliceRoutes[i];
+        for(var j = 0; j < p.length; j++) {
+            var pos = p[j];
+            fill(255);
+            rect(pos.x*unit, pos.y*unit, unit, unit);
+        }
+    }
+}
+
+function checkBoundaries() {
+    if(initPos && (initPos.x+1 > colsSlider.value() || initPos.y+1 > rowsSlider.value())) {
+        initPos = null;
+    }
+
+    if(finalPos && (finalPos.x+1 > colsSlider.value() || finalPos.y+1 > rowsSlider.value())) {
+        finalPos = null;
+    }
+
+    for(var i = walls.length-1; i >= 0; i--) {
+        if(walls[i].x+1 > colsSlider.value() || walls[i].y+1 > rowsSlider.value()) {
+            walls.splice(i,1);
+        }
+    }
+
+    for(var i = allPoliceRoutes.length-1; i >= 0; i--) {
+        var p = allPoliceRoutes[i];
+        for(var j = 0; j < p.length; j++) {
+            var pos = p[j];
+            if(pos.x+1 > colsSlider.value() || pos.y+1 > rowsSlider.value()) {
+                allPoliceRoutes.splice(i,1);
+            }
+        }
+    }
+}
+
 function draw() {
-    background(255);
+    background(50);
+
+    // check size and fits
+    checkBoundaries();
+
+    // draw
     renderMap();
     renderMainChar();
+    renderExit();
+    renderPolice();
 }
